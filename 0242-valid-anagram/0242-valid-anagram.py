c05f448c8ad1ab94d0568fcb9e5767d3533c35f4
Mujib ur Rahman <121819037+mujib2660@users.noreply.github.com>
2025-01-03 18:23:09 +0500

    Time: 15 ms (46.24%), Space: 17.8 MB (31.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,29 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        """
-        Checks if string 't' is an anagram of string 's'.
-
-        Args:
-          s: The first string.
-          t: The second string.
-
-        Returns:
-          True if 't' is an anagram of 's', False otherwise.
-        """
-
-        # If the lengths of the two strings are not equal, they cannot be anagrams.
         if len(s) != len(t):
             return False
 
-        # Create dictionaries to store the frequency of each character in both strings.
-        count_s = {}  # Dictionary to store character counts for string 's'
-        count_t = {}  # Dictionary to store character counts for string 't'
+        countS, countT = {}, {}
 
-        # Count the frequency of each character in both strings.
-        for char in s:  # Iterate through each character in string 's'
-            count_s[char] = count_s.get(char, 0) + 1  # If the character is not in the dictionary, add it with a count of 1. Otherwise, increment its count.
-        for char in t:  # Iterate through each character in string 't'
-            count_t[char] = count_t.get(char, 0) + 1  # If the character is not in the dictionary, add it with a count of 1. Otherwise, increment its count.
-
-        # If both dictionaries have the same key-value pairs (i.e., same character frequencies), then the strings are anagrams.
-        return count_s == count_t
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
