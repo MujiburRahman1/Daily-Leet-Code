371e4bb766ff5ee0c678f0d218e7725366cc89dc
Mujib ur Rahman <121819037+MujiburRahman1@users.noreply.github.com>
2025-07-20 20:22:57 +0500

    Time: 15 ms (85.45%), Space: 17.7 MB (81.27%) - LeetHub

---
+++ 1472-increasing-decreasing-string/1472-increasing-decreasing-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+
+        result = []
+        while len(result) < len(s):
+            # Increasing pass
+            for i in range(26):
+                if count[i] > 0:
+                    result.append(chr(i + ord('a')))
+                    count[i] -= 1
+            # Decreasing pass
+            for i in range(25, -1, -1):
+                if count[i] > 0:
+                    result.append(chr(i + ord('a')))
+                    count[i] -= 1
+
+        return ''.join(result)
