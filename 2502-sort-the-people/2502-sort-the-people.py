4987bec422e74d3d9af750e866e66010108328a1
Mujib ur Rahman <121819037+MujiburRahman1@users.noreply.github.com>
2025-07-21 10:44:42 +0500

    Time: 3 ms (80.5%), Space: 18.4 MB (9.97%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,5 +1,5 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        arr = list(zip(heights, names))
-        arr.sort(reverse = True)
-        return [name for h, name in arr]
+        indices = list(range(len(names)))
+        indices.sort(key = lambda i: -heights[i])
+        return [names[i] for i in indices]
