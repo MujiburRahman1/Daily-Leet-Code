5d13e049bf232ea70c4bc7adf2a7ff4a77ad25b6
Mujib ur Rahman <121819037+MujiburRahman1@users.noreply.github.com>
2025-08-06 22:24:56 +0500

    Time: 1224 ms (97.32%), Space: 39 MB (50.34%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.py
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.py
@@ -1,43 +1,31 @@
 class Solution(object):
     def numOfUnplacedFruits(self, fruits, baskets):
-        n = len(fruits)
-        seg = [0] * (4 * n + 1)
-        def Update(p):
-            seg[p] = max(seg[p << 1], seg[p << 1 | 1])
-        def Build(p, l, r):
-            if l == r:
-                seg[p] = baskets[l]
-                return
-            mid = (l + r) >> 1
-            Build(p << 1, l, mid)
-            Build(p << 1 | 1, mid + 1, r)
-            Update(p)
-        def Assign(x, v, p, l, r):
-            if x < l or x > r:
-                return
-            if l == r:
-                seg[p] = v
-                return
-            mid = (l + r) >> 1
-            Assign(x, v, p << 1, l, mid)
-            Assign(x, v, p << 1 | 1, mid + 1, r)
-            Update(p)
-        def FirstLarger(v, p, l, r):
-            if seg[p] < v:
-                return r + 1
-            if l == r:
-                return r
-            mid = (l + r) >> 1
-            lf = FirstLarger(v, p << 1, l, mid)
-            if lf <= mid:
-                return lf
-            return FirstLarger(v, p << 1 | 1, mid + 1, r)
-        Build(1, 0, n - 1)
-        res = 0
+        n = len(baskets)
+        s = 1
+        while s < n:
+            s *= 2
+        t = [0] * (2 * s)
+        
+        for i in range(n):
+            t[s + i] = baskets[i]
+        for i in range(s - 1, 0, -1):
+            t[i] = max(t[2 * i], t[2 * i + 1])
+        
+        c = 0
         for x in fruits:
-            pos = FirstLarger(x, 1, 0, n - 1)
-            if pos == n:
-                res += 1
+            if t[1] < x:
+                c += 1
             else:
-                Assign(pos, 0, 1, 0, n - 1)
-        return res
+                i = 1
+                while i < s:
+                    if t[2*i] >= x:
+                        i = 2*i
+                    else:
+                        i = 2*i+1
+                j = i - s
+                t[i] = -1
+                i //= 2
+                while i:
+                    t[i] = max(t[2*i], t[2*i+1])
+                    i //= 2
+        return c
